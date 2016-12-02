bb0d157ee31064b799de2199cd3416f3dc13f381
totor59 <victormarechal59@gmail.com>
2016-12-02 10:58:16 +0100

    modify alias in install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ apt-get update
 apt-get install -y nodejs npm
 npm install
 chmod +x ipMap.sh
-echo -e "\nalias whereami='sudo ./~/ipMap/ipMap.sh'" >> ~/.bash_aliases
+echo -e "\nalias whereami='sudo ./ipMap/ipMap.sh'" >> ~/.bashrc
