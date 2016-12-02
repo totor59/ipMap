3631f056be697bd07108a4c2a599400aaf823925
totor59 <victormarechal59@gmail.com>
2016-12-02 10:48:06 +0100

    modify install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd ipMap
 apt-get update
-apt-get install nodejs npm
+apt-get install -y nodejs npm
 npm install
 chmod +x ipMap.sh
-echo -e "\nalias whereami='sudo ./~/ipMap/ipMap.sh'" >> .bash_aliases
+echo -e "\nalias whereami='sudo ./~/ipMap/ipMap.sh'" >> ~/.bash_aliases
